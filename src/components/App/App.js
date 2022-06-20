a1bdfb3e1f987cc1efc6a9a08d05b6d9ccaa71a4
Patrycjusz Sosnowski <patrycjusz.sosnowski@gmail.com>
2022-06-20 23:48:17 +0200

    add startmenu and battle menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,17 @@
 import styles from './styles.module.css'; 
+import { useState } from 'react';
+import { StartMenu, BattleMenu } from 'components';
 
 export const App = () =>  {
-  return (<div className={styles.main}>This is app</div>);
+
+    const [mode, setMode] = useState('start');
+
+    return <div className={styles.main}>
+      {mode === 'start' && <StartMenu onStartClick={() => setMode('battle')}/>}
+      {mode === 'battle' && <BattleMenu/>}
+      {mode === 'gameOver' && <>gameOver mode</>}
+      
+    </div>
 }
 
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export * from './App';
 export * from './Title';
+export * from './StartMenu';
+export * from './BattleMenu';
